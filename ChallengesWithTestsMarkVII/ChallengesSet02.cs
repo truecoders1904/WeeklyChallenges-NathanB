0b04643363fef30e4fff3064a94dd2a9d9a63f4d
Nathan Broom <nathanbroom95@gmail.com>
2019-04-23 10:32:07 -0500

    started weekly challenge 2

---
+++ ChallengesWithTestsMarkVII/ChallengesSet02.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengesWithTestsMarkVII
+{
+    public class ChallengesSet02
+    {
+        public bool CharacterIsALetter(char c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CountOfElementsIsEven(string[] vals)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsNumberEven(int number)
+        {
+           return number % 2 == 0;
+        }
+
+        public bool IsNumberOdd(int num)
+        {
+            return num % 2 != 0;
+        }
+
+        public double SumOfMinAndMax(IEnumerable<double> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetLengthOfShortestString(string str1, string str2)
+        {
+            throw new NotImplementedException();
+            //Math.min
+        }
+
+        public int Sum(int[] numbers)
+        {
+            throw new NotImplementedException();
+            //sum of numbers in integer array
+        }
+
+        public int SumEvens(int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSumOdd(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long CountOfPositiveOddsBelowNumber(long number)
+        {
+            return number / 2; 
+        }
+    }
+}
